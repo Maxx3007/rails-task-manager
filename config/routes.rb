402b980269fe1ca3fc1bf0b63bdd37ce3f9b5afd
Maxim van Hoorn <mvanhoorn1@babson.edu>
2020-02-18 13:30:21 +0100

    added model, controller routes, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+
+#show all
+get 'tasks', to: 'tasks#index'
+
+#read one
+get 'tasks/:id', to: 'tasks#show', as: :task
+
+#create
+post 'tasks', to: 'tasks#create'
+get 'tasks/new', to: 'tasks#new', as: :new_task
+
+#update
+get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+patch 'tasks', to: 'tasks#update'
+
+#destroy
+delete 'tasks/:id', to: 'tasks#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

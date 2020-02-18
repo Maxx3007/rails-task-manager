b59c317530cf2f886ab20441f13fe18d19b78ef0
Maxim van Hoorn <mvanhoorn1@babson.edu>
2020-02-18 15:42:28 +0100

    delete option included and checkboxes added

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,10 @@ get 'tasks/:id', to: 'tasks#show', as: :task
 
 #update
 get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-patch 'tasks', to: 'tasks#update'
+patch 'tasks/:id', to: 'tasks#update'
 
 #destroy
-delete 'tasks/:id', to: 'tasks#destroy'
+delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
